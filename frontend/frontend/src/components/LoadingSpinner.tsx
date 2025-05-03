15c9fb54037109ed01cb65cb13790a4d097f3082
alifrahman <alifrahman2023@gmail.com>
2025-05-03 19:47:06 -0400

    added spinner

--- frontend/frontend/src/components/LoadingSpinner.tsx
+++ frontend/frontend/src/components/LoadingSpinner.tsx
@@ -1,9 +1,10 @@
+import CircularProgress from '@mui/material/CircularProgress';
+
 export default function LoadingSpinner() {
-    return (
-      <div style={{ textAlign: 'center', padding: '50px' }}>
-        <p>Loading...</p>
-        {/* or use an actual spinner image/icon here */}
-      </div>
-    );
-  }
-  
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <CircularProgress />
+      <p>Loading...</p>
+    </div>
+  );
+}
