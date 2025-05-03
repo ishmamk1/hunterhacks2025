f5a2322cc7745c1d74344623d2bec0daea4bf690
rileydrcelik <rileydrcelik@gmail.com>
2025-05-03 19:23:39 -0400

    changed dae to cs

--- frontend/frontend/src/components/Home.tsx
+++ frontend/frontend/src/components/Home.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useMemo } from 'react';
-import daedalusImg from '../assets/daedalus.jpg';
+import daedalusImg from '../assets/cs_lounge.jpeg';
 import eastLibraryImg from '../assets/east_library.jpg';
 import cafeteriaImg from '../assets/cafeteria.jpg';
 import westLobbyImg from '../assets/west_lobby.jpg';
@@ -44,8 +44,8 @@ const FILTER_OPTIONS = [
 const ROOM_MAP = [
   {
     id: 'daedalus_lounge',
-    name: 'Daedalus Lounge',
-    floor: '3rd Floor',
+    name: 'Computer Science Lounge',
+    floor: '10th Floor',
     image: daedalusImg,
     filters: ['Whiteboard', 'Speaking Allowed', 'Group Work'],
   },
