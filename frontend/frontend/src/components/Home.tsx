26f70f70e452848941a23d721f485b7389c61a04
rileydrcelik <rileydrcelik@gmail.com>
2025-05-03 19:16:23 -0400

    added images

--- frontend/frontend/src/components/Home.tsx
+++ frontend/frontend/src/components/Home.tsx
@@ -1,7 +1,13 @@
 import React, { useState, useMemo } from 'react';
-import daedalusImg from '../assets/dae.png';
-import eastLibraryImg from '../assets/east.png';
-import westLobbyImg from '../assets/west.png';
+import daedalusImg from '../assets/daedalus.jpg';
+import eastLibraryImg from '../assets/east_library.jpg';
+import cafeteriaImg from '../assets/cafeteria.jpg';
+import westLobbyImg from '../assets/west_lobby.jpg';
+import east4thLobbyImg from '../assets/4fl_lobby.jpg';
+import east6thLibraryImg from '../assets/6fl_library.jpg';
+import dolcianiCenterImg from '../assets/dolciani.jpg';
+import studentUnionImg from '../assets/student_union.jpg';
+
 
 import {
   Box,
@@ -47,45 +53,56 @@ const ROOM_MAP = [
     id: 'east_library',
     name: 'East Library',
     floor: '3rd Floor',
-    image: daedalusImg,
+    image: eastLibraryImg,
     filters: ['Quiet', 'Silent Zone'],
   },
   {
     id: 'cafeteria',
     name: 'Cafeteria',
     floor: '3rd Floor',
-    image: daedalusImg,
+    image: cafeteriaImg,
     filters: ['Eating Allowed', 'Speaking Allowed'],
   },
   {
     id: 'west_lobby',
     name: 'West 3rd Floor Lobby',
     floor: '3rd Floor',
-    image: daedalusImg,
+    image: westLobbyImg,
     filters: ['Group Work', 'Speaking Allowed'],
   },
   {
     id: 'east_4th_lobby',
     name: 'East 4th Floor Lobby',
     floor: '4th Floor',
-    image: daedalusImg,
+    image: east4thLobbyImg,
     filters: ['Group Work', 'Whiteboard'],
   },
   {
     id: 'east_6th_library',
     name: 'East Library',
     floor: '6th Floor',
-    image: daedalusImg,
+    image: east6thLibraryImg,
     filters: ['Speaking Allowed'],
   },
   {
     id: 'dolciani_center',
     name: 'Dolciani Center',
     floor: '7th Floor (East)',
-    image: daedalusImg,
+    image: dolcianiCenterImg,
     filters: ['Tutoring Zone', 'Whiteboard'],
   },
+  {
+    id: 'student_union',
+    name: 'Student Union',
+    floor: '3rd Floor (Thomas Hunter)',
+    image: studentUnionImg,
+    filters: ['Group Work', 'Speaking Allowed', 'Eating Allowed'],
+  }
 ];
+
+
+  
+
 
 const Home: React.FC = () => {
   const [roomData, setRoomData] = useState<RoomData | null>(null);
